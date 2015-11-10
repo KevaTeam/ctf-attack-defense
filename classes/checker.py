cc6c571eb1ca80c7495e131177b2ce3b0d59aea5
Dmitry Mukovkin <mukovkin@yandex.ru>
2015-11-10 23:04:06 +0600

    add generate, insert flag to db, call file from process

--- classes/checker.py
+++ classes/checker.py
@@ -1,10 +1,20 @@
+import subprocess
+
 __author__ = 'dmitry'
 
 
 class Checker:
+
     def __init__(self):
         pass
 
-    def get(self, host, path_to_program, flag):
-        pass
+    def get(self, host, path_to_program, flag, flag_id):
+        args = (path_to_program, "-c", "somefile.xml", "-d", "text.txt", "-r", "aString", "-f", "anotherString")
+        #Or just:
+        #args = "bin/bar -c somefile.xml -d text.txt -r aString -f anotherString".split()
+        popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+        popen.wait()
+        output = popen.stdout.read()
+        # print(output)
+        print(host, path_to_program, flag, flag_id, output)
 
--- classes/round.py
+++ classes/round.py
@@ -4,9 +4,10 @@ from functions import ConsoleColors as colors
 from classes.checker import Checker
 import random
 import string
+import time
 
 class Round:
-
+    db = {}
     teams = {}
     services = {}
 
@@ -14,7 +15,13 @@ class Round:
 
     round_count = 0
 
+    path_to_checkers = 'checkers/'
+
+    filename_checkers = 'check'
+
+
     def __init__(self, db, config):
+        self.db = db
         self.teams = config.teams
         self.services = config.services
 
@@ -29,10 +36,22 @@ class Round:
         for team in self.teams:
             print(team['name'])
             for service in self.services:
-                flag = self.generate_flags()
-                self.checker.get(team['host'], 'checkers/up.pl', flag)
-                print(service['name'] + flag)
-
+                self.to_service(team, service)
 
     def generate_flags(self):
         return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(33))
+
+    def to_service(self, team, service):
+        flag = self.generate_flags()
+
+        self.db.flags.insert_one({
+            'round': self.round_count,
+            'flag': flag,
+            'flag_id': 'test_flag_id',
+            'teams': team['_id'],
+            'service': service['_id'],
+            'timestamp': time.time()
+        })
+        path = self.path_to_checkers + self.filename_checkers + '_' + str(service['_id'])
+        self.checker.get(team['host'], path, flag, 'test_flag_id')
+
