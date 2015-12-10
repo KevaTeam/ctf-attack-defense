b33d6ad13131902718b64301cd76cec33c65043f
root <Gennady>
2015-12-10 15:41:57 +0600

    flags.py ver.0,1 beta

--- classes/flags.py
+++ classes/flags.py
@@ -33,61 +33,46 @@ class Flags:
 		sock.listen(10)
 		self.conn, self.addr = sock.accept()
 
-		print('connected:', self.addr) # Возможно лишнее
+		print('connected:', self.addr) # Возможно лишнее!!!
 		
-		print(self.addr[0])
 		team = self.db.teams.find_one({'host': self.addr[0]})
-		print(team)
+
 		if not bool(team):
 				self.conn.send(('Who are you?\n Goodbye\n').encode())
 				self.conn.close()
 		else:		
-			komanda = team["name"]
-			self.conn.send(('hello team ' + komanda + '\n').encode())
+
+			self.conn.send(('hello team ' + team["name"] + '\n').encode())
 
 			while True:
 				data = self.conn.recv(1024)
 				data = str(data.rstrip().decode('utf-8'))
-				print (data)                                #Лишнее
-				data1 = self.db.flags.find_one({'flag': data})
-				print(bool(data1))
 
-				if not bool(data1):
+				flag = self.db.flags.find_one({'flag': data})
+				if not bool(flag):
 					self.conn.send(('not found\n').encode())
 					continue
 
+				if flag['team']['name'] == team["name"]:
+					self.conn.send(('It`s your flag, fox\n').encode())
+					continue
 
-				vremia = time.time()
-				print(team)
-				print(data1['service'])
-				data2 = self.db.scoreboard.find_one({ 
-					'team': team, 
-					'service': {
-						'name': data1['service']['name']
-					}
-				})
+				realtime = time.time()
+				self.life = self.life+flag["timestamp"]
+				if self.life <= realtime:
+					self.conn.send(('this flag is too old\n').encode())
+					continue
 
-				print(data2)
-				status = data2["status"]
-				if status == 'up':
-					whose = data1['team']
-					if whose != komanda:
-						print(data2)
-						print(data1)
-						print(bool(data1))
-						if bool(data1):
-							lt = data1["timestamp"]
-							lt = lt+self.life
-							if lt >= vremia:
-								self.conn.send(('received\n').encode())
-							else:
-								self.conn.send(('your flag is too old\n').encode())
-						else:
-							self.conn.send(('not found\n').encode())
-					else:
-						self.conn.send(('It`s your flag, fox\n').encode())
-				else:
-					self.conn.send(('your service don`t up\n').encode())
+				# status = self.db.scoreboard.find_one({ 
+				# 	'team': team, 
+				# 	'service': {
+				# 		'name': flag['service']['name']
+				# 	}
+				# })				
+				# if status["status"] != 'up':
+				# 	self.conn.send(('Up this service & try again later\n').encode())
+
+				self.conn.send(('recived\n').encode())
 
 			self.conn.close()
 			return True
