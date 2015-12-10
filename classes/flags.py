e0b50cf369eb8b9636557367757260ea83500ee5
root <Gennady>
2015-12-10 11:07:44 +0600

    upgrade flag

--- classes/flags.py
+++ classes/flags.py
@@ -16,42 +16,49 @@ class Flags:
 			print(colors.FAIL + 'Error with requests in response' + colors.ENDC)
 			sys.exit(0)
 
-		api = json.loads(response)
+		data = json.loads(response)
 
 		try:
-			self.lifetime = data["response"]["flags"]["lifetime"]
-			self.round_lenght = data["response"]["round_lenght"]
+			lifetime = data["response"]["settings"]["flags"]["lifetime"]
+			round_length = data["response"]["settings"]["round_length"]
 		except Exception:
 			print(colors.FAIL + 'Error with parse in response' + colors.ENDC)
 			sys.exit(0)
-		life = self.lifetime*self.round_lenght
+		self.life = lifetime*round_length
 
 	def recv(self):
+		# try:
 		sock = socket.socket()
 		sock.bind(('', 9090))
 		sock.listen(1)
-		conn, addr = sock.accept()
+		self.conn, addr = sock.accept()
 
 		print('connected:', addr) # Возможно лишнее
+		
 
 		while True:
-				data = conn.recv(1024)
+				data = self.conn.recv(1024)
 				data = str(data.rstrip().decode('utf-8'))
 				print (data)                                #Лишнее
 				# print (self.lifetime)		#Lishnee
-				# print (self.round_lenght)	#lishnee
-				time = time.time()
+				# print (self.round_length)	#lishnee
+				vremia = time.time()
 				# print (time)
-				data1 = self.db.flags.find({'flag': data})
-				print(data1.count())
-				if data1.count():
-					lt = data1.get(timestamp)
-					lt = lt+life
-					if lt <= time:
-						conn.send(('received\n').encode())
+				data1 = self.db.flags.find_one({'flag': data})
+				print(data1)
+				print(bool(data1))
+				if bool(data1):
+					lt = data1["timestamp"]
+					lt = lt+self.life
+					if lt >= vremia:
+						self.conn.send(('received\n').encode())
+					else:
+						self.conn.send(('your flag is too old\n').encode())
 				else:
-					conn.send(('not found\n').encode())
-				for e in data1: #ЛИШНЕЕ
-					print(e)
-		conn.close()
+					self.conn.send(('not found\n').encode())
+
+		self.conn.close()
 		return data
+		# except:
+		# 	print('Something went wrong')
+		# 	self.conn.close()
