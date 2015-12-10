e0162e67317282f6b374cf2822cada27fba9dd25
root <Gennady>
2015-12-10 09:38:58 +0600

    Update flags

--- classes/flags.py
+++ classes/flags.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import socket
+import socket, json, sys, time
 from classes.round import Round
+from urllib.request import urlopen
+from functions import ConsoleColors as colors
 
 class Flags:
 	def __init__(self, db):
 		self.db = db
+		try:
+			response = urlopen("http://api.keva.su/method/jury.get").read().decode('utf8')
+		except Exception:
+			print(colors.FAIL + 'Error with requests in response' + colors.ENDC)
+			sys.exit(0)
+
+		api = json.loads(response)
+
+		try:
+			self.lifetime = data["response"]["flags"]["lifetime"]
+			self.round_lenght = data["response"]["round_lenght"]
+		except Exception:
+			print(colors.FAIL + 'Error with parse in response' + colors.ENDC)
+			sys.exit(0)
+		life = self.lifetime*self.round_lenght
 
 
 	def recv(self):
@@ -15,18 +32,27 @@ class Flags:
 		sock.listen(1)
 		conn, addr = sock.accept()
 
-		print ('connected:', addr)
-		#s = 'close'
+		print ('connected:', addr) # Возможно лишнее
+
 		while True:
 				data = conn.recv(1024)
 				data = str(data.rstrip().decode('utf-8'))
-				print (data)
+				print (data)                                #Лишнее
+				# print (self.lifetime)		#Lishnee
+				# print (self.round_lenght)	#lishnee
+				time = time.time()
+				# print (time)
 				data1 = self.db.flags.find({'flag': data})
 				print(data1.count())
-				if data1.count() != 0:
-					conn.send(('received\n').encode())
+				if data1.count():
+					lt = data1.get(timestamp)
+					lt = lt+life
+					if lt <= time:
+						conn.send(('received\n').encode())
 				else:
 					conn.send(('not found\n').encode())
+				for e in data1: #ЛИШНЕЕ
+					print (e)
 		return data
 
 		conn.close()
