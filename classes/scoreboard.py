72bf7d92d1dda5435848fe120895757041269976
Timur Koyshinov <koyshinov@mail.ru>
2016-02-18 14:00:31 +0600

    table w/o beauty

--- classes/scoreboard.py
+++ classes/scoreboard.py
@@ -23,15 +23,21 @@ class Scoreboard:
 
             sc = {}
             color = {'UP':'success', 'DOWN':'danger', 'CORRUPT':'warning' ,'MUMBLE':'info'}
+
             for item in scoreboard:
                 if item['team']['name'] not in sc:
                        sc[item['team']['name']] = {}
 
                 sc[item['team']['name']][item['service']['name']] = {
                     'status': item['status'],
-                    'message': str(item['message'])
+                    'message': str(item['message']),
+                    'attack': str(item['attack']),
+                    'defense': str(item['defense']),
+                    'up_round': str(item['up_round'])
                 }
+
             print(sc)
+
             return render_template('index.html', scoreboard=sc, color=color)
 
         self.app.debug = True
