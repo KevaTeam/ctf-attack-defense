e180500d1c6573a36050632bf6866b3ab96712f0
Evgenii Sopov <mrseakg@gmail.com>
2016-07-21 01:58:49 +0700

    added previous version of visualization

--- visualization/theme1/js/apigame_emu.js
+++ visualization/theme1/js/apigame_emu.js
@@ -3,18 +3,89 @@ window.ApiGame = new function (){
 	var self = this;
 	self.cache = {};
 	self.cache.teams = [
-		{ 'name': 'team1' },
-		{ 'name': 'team2' },
-		{ 'name': 'team3' },
-		{ 'name': 'team4' },
-		{ 'name': 'team5' },
-		{ 'name': 'team6' },
+		{
+			'name': 'yozik',
+			'logo': 'images/yozik.png',
+			'color': 'white'
+		},
+		{
+			'name': 'keva',
+			'logo': 'images/keva.png',
+			'color': 'yellow'
+		},
+		{
+			'name': 'leetmore',
+			'logo': 'images/leetmore.png',
+			'color': 'pink'
+		},
+		{
+			'name': 'brizz',
+			'logo': 'images/brizz.png',
+			'color': 'red'
+		},
+		{
+			'name': 'censored',
+			'logo': 'images/censored.png',
+			'color': 'green'
+		},
+		{
+			'name': 'curiosity',
+			'logo': 'images/curiosity.png',
+			'color': 'grey'
+		},
+		{
+			'name': 'ufologists',
+			'logo': 'images/ufologists.png',
+			'color': 'purple'
+		},
+		{
+			'name': 'honeypot',
+			'logo': 'images/honeypot.png'
+		},
+		{
+			'name': 'SUSlo.PAS',
+			'logo': 'images/SUSlo.PAS.png',
+			'color': 'lightgray'
+		},
 	];
 	
-	
-	
+	self.cache.stat = []
+
+	for(i in self.cache.teams){
+		var stat = {
+			'name': self.cache.teams[i].name,
+			'score': 0,
+			'flags': []
+		};
+		
+		for(i1 in self.cache.teams){
+			stat['flags'].push({
+				'from': self.cache.teams[i1].name,
+				'count': 0
+			});
+		}
+		self.cache.stat.push(stat)
+	}
+
 	self.teams = function(){
 		return self.cache.teams
+	};
+	
+	self.stat = function(){
+		var d = $.Deferred();
+		// emulation network
+		setTimeout(function(){
+			d.resolve(self.cache.stat);
+			for(i in self.cache.stat){
+				var diff = Math.floor(Math.random()*100);
+				self.cache.stat[i].score += diff;
+				
+				for(i1 in self.cache.stat[i].flags){
+					self.cache.stat[i].flags[i1].count = Math.floor(Math.random()*10) < 7 ? 0 : 1;
+				}
+			}
+		}, 100);
+		return d; 
 	};
 };
 
