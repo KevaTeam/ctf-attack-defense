4ce79531103d5f3401d279317ebcd7178882da22
Dmitry Mukovkin <mukovkin@yandex.ru>
2016-08-27 13:15:32 +0700

    fix setuptools in install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ sudo apt-get install python3 python3-pip -y
 sudo apt-get install mongodb -y
 sudo pip3 install pymongo
 sudo pip3 install requests
+sudo pip install --upgrade pip setuptools
 sudo pip3 install flask
