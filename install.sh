7e96d8f6a9b4c10b4da65574b66b38ead387fa70
Sam Sheffres <ssheffres@gmail.com>
2016-09-28 20:36:46 +0600

    Update Install Script for Mac

--- install.sh
+++ install.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
+if [ "$(uname)" == "Darwin" ]; then
+    # Install for Mac
+    echo Installing for Mac!
 
-sudo apt-get update
-sudo apt-get install python3 python3-pip -y
-sudo apt-get install mongodb -y
-sudo pip3 install pymongo
-sudo pip3 install requests
-sudo pip install --upgrade pip setuptools
-sudo pip3 install flask
+    echo Installing Python 3 and Mongo
+    brew install python3 mongodb
+
+    echo Installing Python Deps - sudo required
+    sudo pip3 install pymongo
+    sudo pip3 install requests
+    sudo pip install --upgrade pip setuptools
+    sudo pip3 install flask
+
+    echo Done!
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Install for Linux
+    echo Installing for Linux!
+
+    echo Updating apt - sudo required!
+    sudo apt-get update
+
+    echo Installing Python 3 and pip3
+    sudo apt-get install python3 python3-pip -y
+
+    echo Installing Mongo
+    sudo apt-get install mongodb -y
+
+    echo Installing Python Deps
+    sudo pip3 install pymongo
+    sudo pip3 install requests
+    sudo pip install --upgrade pip setuptools
+    sudo pip3 install flask
+
+    echo Done!
+fi
